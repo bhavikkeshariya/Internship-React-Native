a7c97e6f0bd38f5a054d4bd99c20cf06bdc860c1
bhavikkeshariya <shyamkeshariya@gmail.com>
2023-02-03 10:34:35 +0530

    Create demo feb3.js

---
+++ src/Feb3.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const Feb3 = () => {
+  <View style={styles.view1}>
+    <Text style={styles.text}>Welcome to</Text>
+  </View>;
+};
